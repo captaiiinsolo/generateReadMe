87eb494428d38e9c6fe61da2aa4109cfd75b5266
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-21 02:08:40 -0800

    add questions

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const generateMarkDown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your GitHub username?", "What is your email address?", "What is your project's name?", "Please wite a short description of your project -", "What kind of license should your project have?", "What command should be run to install dependencies?", "What command should be run to run tests?", "What does the user need to know about using the repo?", "What does the user need to know about contributing to the repo?"];
+
+const [username, email, projectName, description, license, dependencies, tests, repoUsage, contributing] = questions;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
